a824637db1b97f6e1a255921709f50714008ec33
Zain Ahmed <zain.ahmed@ada.ac.uk>
2024-10-23 11:56:55 +0000

    Added main logic to the parseFile function

--- dfp.js
+++ dfp.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
+const { log } = require("console");
+const fs = require("fs");
 
-function parseFile (indata, outdata, delimiter = ';') {
+function parseFile(inputData, outData, delimiter = ";") {
+	const data = fs.readFileSync(inputData, "utf-8");
+	const lines = data.split(/\n/);
 
+	for (let i = 1; i < lines.length; i++) {
+		let line = lines[i].split(delimiter);
+
+		let sentiment = line[1].trim();
+		let review = line[0].trim();
+		review = review.substring(0, 20);
+
+		let newData = `${sentiment};${review}\n`;
+		fs.appendFileSync(outData, newData);
+	}
 }
-
-
-
-  
-
-
 
 // Leave this code here for the automated tests
 module.exports = {
-  parseFile,
-}
+	parseFile,
+};
+
+parseFile("./datafile.csv", "./outputfile.csv");
