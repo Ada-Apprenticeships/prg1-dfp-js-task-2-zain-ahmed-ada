33e3ab47e17b22eb17d1ad871a21d48efd77d814
Zain Ahmed <zain.ahmed@ada.ac.uk>
2024-10-23 12:03:50 +0000

    Passed all test cases

--- dfp.js
+++ dfp.js
@@ -1,7 +1,19 @@
 const { log } = require("console");
 const fs = require("fs");
 
+// function deleteExistingOutputFile() {
+// 	//
+// }
+
 function parseFile(inputData, outData, delimiter = ";") {
+	if (!fs.existsSync(inputData)) {
+		return -1;
+	}
+
+	if (fs.existsSync(outData)) {
+		fs.unlinkSync(outData);
+	}
+
 	const data = fs.readFileSync(inputData, "utf-8");
 	const lines = data.split(/\n/);
 	let count = 0;
@@ -9,11 +21,12 @@ function parseFile(inputData, outData, delimiter = ";") {
 	for (let i = 1; i < lines.length; i++) {
 		let line = lines[i].split(delimiter);
 
+		// const [review, sentiment] = lines[i].split(delimiter).map(item => item.trim())
 		let sentiment = line[1].trim();
 		let review = line[0].trim();
 		review = review.substring(0, 20);
 
-		let newData = `${sentiment};${review}\n`;
+		let newData = `${sentiment}${delimiter}${review}\n`;
 		fs.appendFileSync(outData, newData);
 		count++;
 	}
@@ -25,4 +38,5 @@ module.exports = {
 	parseFile,
 };
 
+//deleteExistingOutputFile();
 parseFile("./datafile.csv", "./outputfile.csv");
